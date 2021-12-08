e73c9d1b9e89cda0415c70f537ecf0669c90d15b
Gustavo Rondello <gustavorondello99@outlook.com>
2021-12-08 19:30:22 -0300

    Feito desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,20 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techArray, username) {
+  let techLearn = [];
+  techArray.sort();
+  if (techArray && username != null) {
+    for (let i = 0; i < techArray.length; i += 1) {
+      let nameTech = techArray[i];
+      techLearn.push({
+        tech: nameTech,
+        name: username,
+      });
+    }
+  }
+  if (techArray.length === 0) {
+    return "Vazio!";
+  }
+  return techLearn;
 }
 
 // Desafio 11
